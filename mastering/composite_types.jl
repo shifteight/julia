c78f345057f351f6bd01da4d6add50af2e7872a0
Kevin Qian <yanxiang.qian@gmail.com>
2015-11-27 15:12:40 +0800

    mastering

---
+++ mastering/composite_types.jl
@@ -0,0 +1,11 @@
+type Member
+    fullname::ASCIIString
+    email::ASCIIString
+    meetup::ASCIIString
+    age::Int
+    organiser::Bool
+    mobile::ASCIIString
+end
+
+me = Member("Malcolm Sherrington", "malcolm@ljuug.org", "London Julia User
+Group", 55, true, "07777 5555555")
