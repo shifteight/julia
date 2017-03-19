d7508bced68c2c3ff36b554300e6269782dc47ef
Kevin Qian <scifi.org@gmail.com>
2017-03-19 22:03:06 +0800

    plot_demo

---
+++ plot_demo.jl
@@ -0,0 +1,10 @@
+using RDatasets
+iris = dataset("datasets", "iris")
+
+using StatPlots
+
+scatter(iris, :SepalLength, :SepalWidth, group=:Species,
+        title = "My awesome plot",
+        xlabel = "Length", ylabel = "Width",
+        m=(0.5, [:cross :hex :star7], 12),
+        bg=RGB(.2,.2,.2))
