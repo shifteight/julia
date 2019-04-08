2c37d7c3c47ebdfaeff79d08f51846e0be34e63c
Kevin Qian <scifi.org@gmail.com>
2019-04-08 13:58:50 +0800

    add two scripts

---
+++ scripts/hailstone.jl
@@ -0,0 +1,20 @@
+using PyPlot
+
+function hailLength(x::Int)
+    n = 0
+    while x != 1
+        if x % 2 == 0
+            x = Int(x / 2)
+        else
+            x = 3x + 1
+        end
+        n += 1
+    end
+    return n
+end
+
+lengths = [hailLength(x0) for x0 in 2:10^7]
+
+plt[:hist](lengths, 1000, normed = "true")
+xlabel("Length")
+ylabel("Frequency");
